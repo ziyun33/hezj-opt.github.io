a7bd3356de90c825f85a961a49ede63e3be895ad
Zijun He <zijun_he@zju.edu.cn>
2023-01-31 17:11:42 +0800

    Site updated: 2023-01-31 17:11:40

--- js/baidu-tongji.js
+++ js/baidu-tongji.js
@@ -1,7 +1,15 @@
+// var _hmt = _hmt || [];
+// (function() {
+//   var hm = document.createElement("script");
+//   hm.src = "https://hm.baidu.com/hm.js?";
+//   var s = document.getElementsByTagName("script")[0]; 
+//   s.parentNode.insertBefore(hm, s);
+// })();
+
 var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?";
+  hm.src = "https://hm.baidu.com/hm.js?a769746b31b95e934300c2470879062d";
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 })();
